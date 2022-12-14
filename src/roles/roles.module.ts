045dbb6e8bdb3450311bfe8f595b3a594ad322f9
toantv <toantv@newwave.com.vn>
2022-12-14 11:50:11 +0700

    swagger module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
 import { userProvider } from './providers/role.provider';
 import { RoleRepository } from './repositories/role.repository';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RolesController],
   providers: [RolesService, ...userProvider, RoleRepository],
 })
